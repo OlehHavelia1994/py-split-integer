95f9e282fec99b293aa8be9c0d746bb75f77df60
Oleg <oleggavelya@gmail.com>
2025-11-19 18:53:36 +0200

    add assert equal val and add sum(res) == val

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -9,12 +9,15 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     value = 6
     number_of_parts = 2
     res = split_integer(value, number_of_parts)
-    assert len(res) == number_of_parts and max(res) == min(res)
+    assert len(res) == number_of_parts
+    assert sum(res) == value
+    assert res == [3,3]
+
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    number_of_parts = 2
-    value = 6
+    number_of_parts = 1
+    value = 8
     res = split_integer(value, number_of_parts)
     assert len(res) == number_of_parts
     assert sum(res) % len(res) == 0
@@ -28,8 +31,8 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    number_of_parts = 2
-    value = 6
+    number_of_parts = 5
+    value = 3
     res = split_integer(value, number_of_parts)
     assert max(res) - min(res) <= 1
     assert len(res) == number_of_parts
