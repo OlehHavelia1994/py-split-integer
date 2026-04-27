70719d7803d02d039eedf81b49966d7d87f0bb96
Oleg <oleggavelya@gmail.com>
2026-04-27 21:14:31 +0300

    refactor module test-split-integer add example in func test_sum and test_sort

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,26 +2,18 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
+    assert sum(split_integer(8, 1)) == 8
+    assert sum(split_integer(6, 2)) == 6
+    assert sum(split_integer(17, 4)) == 17
     assert sum(split_integer(32, 6)) == 32
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
-    # value = 6
-    # number_of_parts = 2
-    # res = split_integer(value, number_of_parts)
-    # assert len(res) == number_of_parts
-    # assert sum(res) == value
-    # assert res == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
-    # number_of_parts = 1
-    # value = 8
-    # res = split_integer(value, number_of_parts)
-    # assert len(res) == number_of_parts
-    # assert sum(res) % len(res) == 0
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -29,6 +21,7 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
     value = 32
     res = split_integer(value, number_of_parts)
     assert res == sorted(res)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
